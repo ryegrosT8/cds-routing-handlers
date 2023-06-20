d9f72323f771ccb4ad2613399fb500d2b6912626
ryegrost8 <rafael.yegros@bit4b.com.br>
2023-06-20 11:05:09 -0300

    feat: Add after and before handler for actions

--- src/decorators/method/Action.ts
+++ src/decorators/method/Action.ts
@@ -20,3 +20,41 @@ export function Action(name: string): MethodDecorator {
         });
     };
 }
+
+/**
+ * Before Action handler decorator.
+ *
+ * @export
+ * @param {string} name Name of the action
+ * @returns {MethodDecorator}
+ */
+export function BeforeAction(name: string): MethodDecorator {
+    return (target: Object, key: string | symbol, descriptor: PropertyDescriptor) => {
+        getMetadataArgsStorage().addActionMetadata({
+            target: target.constructor,
+            method: key as string,
+            handler: HandlerType.Before,
+            operation: ODataOperation.Action,
+            functionImportName: name,
+        });
+    };
+}
+
+/**
+ * After Action decorator.
+ *
+ * @export
+ * @param {string} name Name of the action
+ * @returns {MethodDecorator}
+ */
+export function AfterAction(name: string): MethodDecorator {
+    return (target: Object, key: string | symbol, descriptor: PropertyDescriptor) => {
+        getMetadataArgsStorage().addActionMetadata({
+            target: target.constructor,
+            method: key as string,
+            handler: HandlerType.On,
+            operation: ODataOperation.Action,
+            functionImportName: name,
+        });
+    };
+}
